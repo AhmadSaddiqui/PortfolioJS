7e043d383bfe34891515164c493b5d1df775b3e9
ahmad <ahmadsaddiqui00@gmail.com>
2024-08-09 11:13:37 -0700

    Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: [ "App Developer", "Frontend Developer", "UI Designer"],
+        typeSpeed: 90,
+        backSpeed: 50,
+        backDelay: 500,
+        loop: true
+    });
+});
